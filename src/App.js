210b5059d9f20eb18cfca5162da2abe7e4d994f4
guillermo <guillaumeraez@gmail.com>
2021-06-22 16:01:48 +0200

    Inialized react repo and installed dependencies such as redux, router-dom, axios and sass.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import 
+
+import './App.scss';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Home from './containers/Home/Home';
+import Login from './containers/Login/Login';
 
 function App() {
   return (
     <div className="App">
+
+      <BrowserRouter>
+        <Switch>
+
+          <Route path='/' exact component={Home}/>
+          <Route path='/login' exact component={Login}/>
+
+        </Switch>
+
+      </BrowserRouter>
       
     </div>
   );
